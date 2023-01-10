6e1457b5ab8e8730f776435fb0c4e01be02eaf5a
Tien Khanh <nguyentienkhanh2911@gmail.com>
2023-01-11 03:59:50 +0700

    Update theme

--- monkey-blogging/src/App.js
+++ monkey-blogging/src/App.js
@@ -10,10 +10,65 @@ function App() {
     <div>
       <AuthProvider>
         <Routes>
-          <Route path="/" element={<HomePage />}></Route>
-          <Route path="/sign-up" element={<SignUpPage />}></Route>
-          <Route path="/sign-in" element={<SignInPage />}></Route>
-          <Route path="*" element={<NotFoundPage />}></Route>
+          <Route path="/" element={<HomePage></HomePage>}></Route>
+          <Route path="/sign-up" element={<SignUpPage></SignUpPage>}></Route>
+          <Route path="/sign-in" element={<SignInPage></SignInPage>}></Route>
+
+          <Route path="*" element={<NotFoundPage></NotFoundPage>}></Route>
+          {/* <Route
+            path="/category/:slug"
+            element={<CategoryPage></CategoryPage>}
+          ></Route>
+          <Route
+            path="/:slug"
+            element={<PostDetailsPage></PostDetailsPage>}
+          ></Route>
+          <Route element={<DashboardLayout></DashboardLayout>}>
+            <Route
+              path="/dashboard"
+              element={<DashboardPage></DashboardPage>}
+            ></Route>
+            <Route
+              path="/manage/posts"
+              element={<PostManage></PostManage>}
+            ></Route>
+            <Route
+              path="/manage/add-post"
+              element={<PostAddNew></PostAddNew>}
+            ></Route>
+            <Route
+              path="/manage/update-post"
+              element={<PostUpdate></PostUpdate>}
+            ></Route>
+            <Route
+              path="/manage/category"
+              element={<CategoryManage></CategoryManage>}
+            ></Route>
+            <Route
+              path="/manage/add-category"
+              element={<CategoryAddNew></CategoryAddNew>}
+            ></Route>
+            <Route
+              path="/manage/update-category"
+              element={<CategoryUpdate></CategoryUpdate>}
+            ></Route>
+            <Route
+              path="/manage/user"
+              element={<UserManage></UserManage>}
+            ></Route>
+            <Route
+              path="/manage/add-user"
+              element={<UserAddNew></UserAddNew>}
+            ></Route>
+            <Route
+              path="/manage/update-user"
+              element={<UserUpdate></UserUpdate>}
+            ></Route>
+            <Route
+              path="/profile"
+              element={<UserProfile></UserProfile>}
+            ></Route>
+          </Route> */}
         </Routes>
       </AuthProvider>
     </div>
